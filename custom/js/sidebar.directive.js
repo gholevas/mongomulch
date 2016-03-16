a3a0831cd9ff5294f9be2245dfe1bcc303c99ed4
George <gholevas1@gmail.com>
2016-03-16 18:58:11 -0400

    working on local storage

--- custom/js/sidebar.directive.js
+++ custom/js/sidebar.directive.js
@@ -13,10 +13,26 @@ app.controller("SideBarCtrl", function($scope) {
     $scope.schemas = ['Users'];
     $scope.schemaName = '';
 
+    // storage.keys(function(error, keys) {
+    //     if (error) throw error;
+
+    //     console.log(keys)
+    // });
+
+    // storage.get('schemas2', function(error, data) {
+    //     if (error) throw error;
+
+    //     console.log(data);
+    // });
+    // console.log()
     $scope.addSchema = function() {
-    	// storage.set('schemas',{name: $scope.schemaName},function(error){
-    	// 	if(error) throw error;
-    	// })
+        // storage.get('schemas', function(error, data) {
+        //     if (error) throw error;
+        //     console.log(data);
+        // });
+        // storage.set('schemas2', [{ name: $scope.schemaName }], function(error) {
+        //     if (error) throw error;
+        // })
         $scope.schemas.push($scope.schemaName);
         $scope.schemaName = '';
     }
