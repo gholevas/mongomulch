6d4f2a8de05e5b1377748a49ae3964f3660f74bd
sp6pe <sp6pe@virginia.edu>
2016-03-22 21:16:56 -0400

    changed schema object

--- custom/right_side_nav/right-side-nav.directive.js
+++ custom/right_side_nav/right-side-nav.directive.js
@@ -8,36 +8,77 @@ app.directive('rightSideNav', function() {
 
 
 app.controller('RightSideNavCtrl', function($scope, $timeout, $mdSidenav, SchemaFactory) {
-
+        console.log($scope.schemas);
     function resetField() {
         $scope.field = {
-            options: []
+            options: {}
         };
+
+        $scope.defaultValue ='';
     }
     resetField();
     //if array scope.field.type is [String]
 
     $scope.saveField = function() {
        // console.log($scope.field);
-        //console.log($scope.selectedArrType)
-        if($scope.field.type = "Array of...") $scope.field.type = '['+$scope.selectedArrType+']';
-        $scope.schema.addField($scope.field)
-        resetField();
-        $mdSidenav('right').close()
+        
+
+        if($scope.field.type === "Array of..."){
+            $scope.field.type = '['+$scope.selectedArrType+']';
+            $scope.schema.addField($scope.field)
+            resetField();
+            $mdSidenav('right').close()
+        } 
+        else if($scope.field.type === 'Embed...') {
+            $scope.field.type = '['+$scope.selectedEmbed+']';
+            $scope.schema.addField($scope.field)
+            resetField();
+            $mdSidenav('right').close()
+        } 
+        else if($scope.field.type === 'Reference to...') {
+            $scope.field.type = 'mongoose.Schema.Types.ObjectId';
+            $scope.field.options['ref'] = $scope.selectedSchema;
+            $scope.schema.addField($scope.field)
+            resetField();
+            $mdSidenav('right').close()
+        } 
+        else{
+             if($scope.defaultValue){
+                $scope.field.options['default'] = $scope.defaultValue;
+                $scope.schema.addField($scope.field);
+                resetField();
+                $mdSidenav('right').close();
+
+            } else{
+                 $scope.schema.addField($scope.field);
+                resetField();
+               $mdSidenav('right').close();
+
+            }
+           
+          
+        }
+            //$scope.field.type = '['+$scope.selectedEmbed+']';å
+        
     }
 
 
     $scope.typeArr = ['String', 'Number', 'Boolean', 'Buffer', 'Date', 'Array of...', 'Reference to...', 'Embed...'];
     $scope.arrOf = ['String', 'Number', 'Boolean', 'Date', 'Buffer'];
     $scope.possibleOptions = ['Unique', 'Required', 'Select', 'Sparse', 'Text'];
-
+    
     $scope.toggle = function(option) {
-        var idx = $scope.field.options.indexOf(option);
-        if (idx > -1) $scope.field.options.splice(idx, 1);
-        else $scope.field.options.push(option);
+        if($scope.field.options[option]) delete $scope.fields.options[option];
+        else $scope.field.options[option] = true;
+
+
+        // var idx = $scope.field.options.indexOf(option);
+        // if (idx > -1) $scope.field.options.splice(idx, 1);
+        // else $scope.field.options.push(option);
     };
+
     $scope.exists = function(option) {
-        return $scope.field.options.indexOf(option) > -1;
+        return $scope.field.options[option] != null;
     };
 
 
